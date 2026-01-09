c72a62690a19ae2a717364cce25ff04f5ea04bb4
MbuguaIT <jacobmbuguait@gmail.com>
2026-01-09 12:40:03 +0300

    Update Maziwa tele Sms credentials: API KEY

--- src/integrations/lancola-sms/lancola-sms.config.ts
+++ src/integrations/lancola-sms/lancola-sms.config.ts
@@ -2,7 +2,7 @@ import { ConfigService } from '@nestjs/config';
 
 export const getLancolaSmsConfig = (configService: ConfigService) => ({
   API_URL: configService.get('LANCOLA_SMS_APIURL', 'https://sms.lancolatech.com/api/services/sendsms/?apikey='),
-  API_KEY: configService.get('LANCOLA_SMS_apiKey', '0b945c04c2269802975acbab57c91eb6'),
+  API_KEY: configService.get('LANCOLA_SMS_apiKey', 'dde2efa9e40d31eae58cd7b1f89c139e'),
   PARTNER_ID: configService.get('LANCOLA_SMS_partnerID', '8029'),
   SHORT_CODE: configService.get('LANCOLA_SMS_shortCode', 'Maziwa Tele'),
 });
--- src/integrations/lancola-sms/services/lancola-sms/lancola-sms.service.ts
+++ src/integrations/lancola-sms/services/lancola-sms/lancola-sms.service.ts
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
-import { Injectable, BadRequestException } from '@nestjs/common';
+import { Injectable, BadRequestException, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { getRequest } from '../http';
 import { phoneNumberWithCountryCode, prepareMessage } from './lancola-sms.functions';
@@ -7,14 +7,22 @@ import { SMSInterface } from './sms.interface';
 
 @Injectable()
 export class LancolaSmsService {
+  private readonly logger = new Logger(LancolaSmsService.name);
+
   constructor(private readonly configService: ConfigService) {}
 
   async sendSMS(payload: SMSInterface) {
     try {
       const phoneNumber = phoneNumberWithCountryCode({ phoneNumber: payload.phone, countryCode: '254' });
       payload.phone = phoneNumber;
+      this.logger.log(`[SMS] Normalized phone number to: ${phoneNumber}`);
+      
       const finalURL = prepareMessage(payload, this.configService);
+      this.logger.log(`[SMS] Sending SMS to provider for phone: ${phoneNumber}`);
+      
       const response = await getRequest(finalURL);
+      this.logger.log(`[SMS] Provider response: ${JSON.stringify(response)}`);
+      
       return response;
     } catch (error) {
       throw new BadRequestException(`Failed to send SMS: ${error.message}`);
